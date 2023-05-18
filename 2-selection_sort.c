cb1553e0514ecb57a9315a0fb85b16ecf0aea292
lataba <ltballeste@gmail.com>
2023-05-17 21:04:45 -0700

    first version with a segmentation fault to solve

---
+++ 2-selection_sort.c
@@ -0,0 +1,28 @@
+#include "sort.h"
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * selection_sort - function that sorts an array of integers
+ * @array: an array to be sorted
+ * @size: the size of the array
+ */
+
+void selection_sort(int *array, size_t size)
+
+{
+	size_t i, j, min_i;
+	int temp;
+
+	for (i = size; i < size - 1; i++)
+	{
+		min_i = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min_i])
+				min_i = j;
+		}
+	}
+	temp = array[i];
+	array[i] = array[min_i];
+	array[min_i] = temp;
+}
