fab251ab426dba7a8c56d107d8b66c5eb5934fa8
lataba <ltballeste@gmail.com>
2023-05-19 21:35:08 -0700

    fixing printing duplicated lines

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -13,18 +13,26 @@ void selection_sort(int *array, size_t size)
 	size_t i, j, k, i_min;
 	int temp;
 
+	if (array == NULL)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		i_min = i;
 		for (j = i + 1; j < size; j++)
+		{
 			if (array[j] < array[i_min])
 				i_min = j;
-		temp = array[i_min];
-		array[i_min] = array[i];
-		array[i] = temp;
-		for (k = 0; k < size - 1; k++)
-			printf("%d, ", array[k]);
-		printf("%d", array[size - 1]);
-		printf("\n");
+		}
+		temp = array[i];
+		array[i] = array[i_min];
+		array[i_min] = temp;
+		if (i != i_min)
+		{
+			for (k = 0; k < size - 1; k++)
+				printf("%d, ", array[k]);
+			printf("%d", array[size - 1]);
+			printf("\n");
+		}
 	}
 }
