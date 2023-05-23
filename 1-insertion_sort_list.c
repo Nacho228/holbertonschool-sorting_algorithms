9cec5d76017a3c531465dfe613088dad319fe817
Nacho228 <6319@holbertonstudents.com>
2023-05-23 05:40:42 -0700

    insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -6,12 +6,12 @@
 */
 void insertion_sort_list(listint_t **list)
 {
-listint_t *current = (*list)->next, *prev, *temp;
+listint_t *current, *prev, *temp;
 
-	if (!(*list) || !(*list)->next)
+	if (list == NULL)
 		return;
 
-
+current = (*list)->next;
 	while (current)
 	{
 		prev = current->prev;
